55bd03e922eee7b231e709047dc6cda6e3ecb6fa
Benjamin Van Iseghem <benjamin.vaniseghem.w3891@student.hogent.be>
2019-02-21 13:19:09 +0100

    added different loggers

--- logger/logger.go
+++ logger/logger.go
@@ -5,11 +5,13 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var kafkaLogger = createKafkaLogger()
+var infoLogger = createInfoLogger()
+var warnLogger = createWarningLogger()
+var errLogger = createErrorLogger()
 
 //Info logs info messages
 func Info(msg string) {
-	kafkaLogger.WithFields(logrus.Fields{
+	infoLogger.WithFields(logrus.Fields{
 		"id":           1,
 		"info field 2": "some value",
 		"info field 3": "another value",
@@ -18,7 +20,7 @@ func Info(msg string) {
 
 //Warning logs warning messages
 func Warning(msg string) {
-	kafkaLogger.WithFields(logrus.Fields{
+	warnLogger.WithFields(logrus.Fields{
 		"id":              2,
 		"warning field 2": "some warning",
 		"warning field 3": "another warning value",
@@ -27,14 +29,15 @@ func Warning(msg string) {
 
 //Error logs error messages
 func Error(msg string) {
-	kafkaLogger.WithFields(logrus.Fields{
+	errLogger.WithFields(logrus.Fields{
 		"id":            3,
 		"error field 2": "some error",
 		"error field 3": "another error value",
 	}).Error(msg)
 }
 
-func createKafkaLogger() *logrus.Logger {
+//Create a logger that uses a hook to post logs to kafka on the "info" topic
+func createInfoLogger() *logrus.Logger {
 	var tempLogger = logrus.New()
 	tempLogger.SetReportCaller(true)
 
@@ -48,6 +51,62 @@ func createKafkaLogger() *logrus.Logger {
 		},
 		[]string{"192.168.99.222:9092"},
 		"info")
+	//Error handling for the creating of the hook
+	if err != nil {
+		logrus.Error("Problem with kafka hook")
+	}
+	//Add hook to the tempLogger
+	tempLogger.Hooks.Add(hook)
+
+	//Debug log
+	logrus.Debug("created logger")
+
+	return tempLogger
+}
+
+//Create a logger that uses a hook to post logs to kafka on the "warning" topic
+func createWarningLogger() *logrus.Logger {
+	var tempLogger = logrus.New()
+	tempLogger.SetReportCaller(true)
+
+	//Create hook using logrus-kafka library
+	//Params are id, severity levels that will be passed, formatter, kafka broker, kafka topic
+	hook, err := lkh.NewKafkaSyncHook(
+		"kh",
+		[]logrus.Level{logrus.InfoLevel, logrus.WarnLevel, logrus.ErrorLevel},
+		&logrus.JSONFormatter{
+			TimestampFormat: "2006-01-02 15:04:05",
+		},
+		[]string{"192.168.99.222:9092"},
+		"warning")
+	//Error handling for the creating of the hook
+	if err != nil {
+		logrus.Error("Problem with kafka hook")
+	}
+	//Add hook to the tempLogger
+	tempLogger.Hooks.Add(hook)
+
+	//Debug log
+	logrus.Debug("created logger")
+
+	return tempLogger
+}
+
+//Create a logger that uses a hook to post logs to kafka on the "error" topic
+func createErrorLogger() *logrus.Logger {
+	var tempLogger = logrus.New()
+	tempLogger.SetReportCaller(true)
+
+	//Create hook using logrus-kafka library
+	//Params are id, severity levels that will be passed, formatter, kafka broker, kafka topic
+	hook, err := lkh.NewKafkaSyncHook(
+		"kh",
+		[]logrus.Level{logrus.InfoLevel, logrus.WarnLevel, logrus.ErrorLevel},
+		&logrus.JSONFormatter{
+			TimestampFormat: "2006-01-02 15:04:05",
+		},
+		[]string{"192.168.99.222:9092"},
+		"Error")
 	//Error handling for the creating of the hook
 	if err != nil {
 		logrus.Error("Problem with kafka hook")
