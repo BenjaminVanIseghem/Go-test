ada61ecb5b1381bced71738e93d2ee83e28ac455
Benjamin Van Iseghem <benjamin.vaniseghem.w3891@student.hogent.be>
2019-02-21 11:37:48 +0100

    infoLogger finished

--- log-spawner.go
+++ log-spawner.go
@@ -2,13 +2,11 @@ package main
 
 //All imports necessary for the logging chain
 import (
+	"log-spawner/logger"
 	"math/rand"
-
-	lkh "github.com/arriqaaq/logrus-kafka"
-	log "github.com/sirupsen/logrus"
 )
 
-var infoLogger = createInfoLogger()
+var infoLogger = logger.CreateInfoLogger()
 
 //Initiliazing of the logrus object
 func init() {
@@ -20,38 +18,6 @@ func main() {
 		num := rand.Intn(100)
 		infoLogger.Infof("Info number: %d", num)
 	}
-}
-
-func createInfoLogger() *log.Entry {
-	var tempLogger = log.New()
-	//Create hook using logrus-kafka library
-	//Params are id, severity levels that will be passed, formatter, kafka broker, kafka topic
-	infoHook, err := lkh.NewKafkaSyncHook(
-		"kh",
-		[]log.Level{log.InfoLevel, log.WarnLevel, log.ErrorLevel},
-		&log.JSONFormatter{
-			TimestampFormat: "2006-01-02 15:04:05",
-		},
-		[]string{"192.168.99.222:9092"},
-		"info")
-	//Error handling for the creating of the hook
-	if err != nil {
-		log.Error("Problem with kafka hook")
-	}
-
-	//Add hook to temporary logger object
-	tempLogger.Hooks.Add(infoHook)
-
-	//Create infologger
-	infoLogger := tempLogger.WithFields(log.Fields{
-		"chain_id": "Which part of the chain",
-		"service":  "service name",
-		"event_id": 1,
-	})
-	//Debug log
-	log.Debug("created info logger")
-
-	return infoLogger
 }
 
 //EXAMPLES OF ALL SEVERITY LEVELS OF LOGRUS
