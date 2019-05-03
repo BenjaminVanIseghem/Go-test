5c617c44643be7695ca6415dc9fa1f89e3e05a8d
Benjamin Van Iseghem <benjamin.vaniseghem.w3891@student.hogent.be>
2019-05-03 14:03:35 +0200

    log to file 1

--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"errors"
+	"os"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	w, err := os.Create("/Users/benjaminvaniseghem/Documents/logs/logfile1.log")
+	if err != nil {
+		panic(err)
+	}
+	logger := logrus.New()
+	logger.SetOutput(w)
 	for {
-		logrus.Info("Info message")
-		logrus.Warn("Warning message")
-		logrus.Error("Error message", errors.New("Error"))
+		logger.Info("Info message")
+		logger.Warn("Warning message")
+		logger.Error("Error message", errors.New("Error"))
+		time.Sleep(1200 * time.Millisecond)
 	}
 }
