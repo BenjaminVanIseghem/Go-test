fd4d10e5f991865dec776239bec164c814ebe558
Benjamin Van Iseghem <benjamin.vaniseghem.w3891@student.hogent.be>
2019-05-10 11:01:00 +0200

    logrus implementation

--- main.go
+++ main.go
@@ -2,23 +2,28 @@ package main
 
 import (
 	"errors"
-	"os"
 	"time"
 
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	w, err := os.Create("/Users/benjaminvaniseghem/Documents/logs/logfile1.log")
-	if err != nil {
-		panic(err)
-	}
-	logger := logrus.New()
-	logger.SetOutput(w)
+	// w, err := os.Create("/Users/benjaminvaniseghem/Documents/promtail/logs/logfile2.log")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// logger := logrus.New()
+	// logger.SetOutput(w)
+	// for {
+	// 	logger.Info("Info 2 message")
+	// 	logger.Warn("Warning 2 message")
+	// 	logger.Error("Error 2 message", errors.New("Error"))
+	// 	time.Sleep(1200 * time.Millisecond)
+	// }
 	for {
-		logger.Info("Info message")
-		logger.Warn("Warning message")
-		logger.Error("Error message", errors.New("Error"))
+		logrus.Info("Info message")
+		logrus.Warn("Warning message")
+		logrus.Error("Error message", errors.New("Error"))
 		time.Sleep(1200 * time.Millisecond)
 	}
 }
